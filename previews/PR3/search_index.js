c1cf8b65edb8cc56d8432f77b3e293fc6bb9d9aa
Documenter.jl <documenter@juliadocs.github.io>
2026-06-19 05:48:55 +0000

    build based on 64ee9c8

---
+++ previews/PR3/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"category":"section","location":"#Pinax.jl","page":"Home","text":"","title":"Pinax.jl"},{"category":"section","location":"#Models","page":"Home","text":"","title":"Models"}]
+}
